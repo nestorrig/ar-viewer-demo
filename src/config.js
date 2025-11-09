9dd754492d62de4476b0ec4637e1b4d8654b9921
Nestor Rios <neriga10@hotmail.com>
2025-11-09 12:34:26 -0600

    feat:add more models, fix alpura model

--- src/config.js
+++ src/config.js
@@ -1,14 +1,33 @@
 export const models = [
   // Alpura
   {
-    name: "CartLecheAlp",
-    path: "/models/alpura/CartLecheAlp.glb",
-    usdz: "/models/alpura/CartLecheAlp.usdz",
+    name: "alpura",
+    path: "/models/alpura/alpura.glb",
+    usdz: "/models/alpura/alpura.usdz",
     textures: [
       "/models/alpura/textures/etiqueta01.jpg",
       "/models/alpura/textures/etiqueta02.jpg",
       "/models/alpura/textures/etiqueta03.jpg",
     ],
     poster: "/img/poster.webp",
+  },
+  // helmet
+  {
+    name: "DamagedHelmet",
+    path: "/models/helmet/DamagedHelmet.glb",
+    // usdz: "/models/helmet/DamagedHelmet.usdz",
+    // textures: [
+    //   "/models/helmet/textures/etiqueta01.jpg",
+    //   "/models/helmet/textures/etiqueta02.jpg",
+    //   "/models/helmet/textures/etiqueta03.jpg",
+    // ],
+    poster: "/models/helmet/poster.webp",
+  },
+  // astronaut
+  {
+    name: "Astronaut",
+    path: "/models/astronaut/Astronaut.glb",
+    // usdz: "/models/Astronaut/astronaut.usdz",
+    poster: "/models/astronaut/poster.webp",
   },
 ];
